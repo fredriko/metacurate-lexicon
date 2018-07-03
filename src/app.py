47451b93111f406df3027cdae3f6d4dd924c22fb
fredriko <fredrik.olsson@gmail.com>
2018-07-03 20:19:04 +0200

    Added web interface. Prepared for heroku deployment.

--- src/app.py
+++ src/app.py
@@ -1,44 +1,32 @@
 import gensim
-from flask import Flask, render_template
-from typing import Union
-
-import src.scripts.config as config
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-def print_result(model: Union[gensim.models.FastText, gensim.models.Word2Vec], word: str, top_n: int) -> None:
-    results = model.wv.most_similar(positive=word.replace(" ", "_"), topn=top_n)
-    print("Most similar to '{}'".format(word))
-    fixed = []
-    fixed.append("<ol>\n")
-
-    for result in results:
-        text = str(result[0])
-        score = result[1]
-        text = text.replace("_", " ")
-        print(text, score)
-        fixed.append("<li>" + text + ": " + str(score))
-    print(" ")
-    fixed.append("</ol>\n")
-    return " ".join(fixed)
-
-
-MODEL = gensim.models.Word2Vec.load(config.WORDSPACE_MODELS_DIRECTORY + "fasttext-metacurate-cbow.model")
-
+#MODEL = gensim.models.Word2Vec.load(config.WORDSPACE_MODELS_DIRECTORY + "fasttext-metacurate-cbow.model")
+MODEL = gensim.models.Word2Vec.load("fasttext-metacurate-cbow.model")
 
 @app.route("/")
 def index():
-    #model = gensim.models.Word2Vec.load(config.WORDSPACE_MODELS_DIRECTORY + "fasttext-metacurate-cbow.model")
-    #print_result(model)
-    return "<a href=\"/lookup/gensim\">lookup!</a>"
+    return render_template("home.jinja2")
 
 
-@app.route("/lookup/<string:term>")
-def lookup(term=None):
-    if term is not None:
-        results =   print_result(MODEL, term, 10)
-        return f"seeing {term}" + results
+@app.route("/lookup", methods=["GET", "POST"])
+def lookup():
+    result = []
+    term = None
+    error = None
+    if request.method == "POST":
+        term: str = request.form["term"]
+        term = term.lower().strip()
+        if len(term) > 0:
+            similarities = MODEL.wv.most_similar(positive=term.replace(" ", "_"), topn=10)
+            for similarity in similarities:
+                text = str(similarity[0]).replace("_", " ")
+                score = str(similarity[1])
+                result.append({"term": text, "score": score})
+        else:
+            error = "No term specified!"
+    return render_template("home.jinja2", data={"lookup": term, "similarities": result}, error=error)
 
-    else:
-        return "Please supply term!"
