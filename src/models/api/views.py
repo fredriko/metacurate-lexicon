04e5208bb56e6640303e193dde5d9c6032baedc5
fredriko <fredrik.olsson@gmail.com>
2018-07-06 00:59:08 +0200

    More styling.

--- src/models/api/views.py
+++ src/models/api/views.py
@@ -3,24 +3,31 @@ from flask_restplus import Resource, Api
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 
-api = Api(api_blueprint, version="1.0", title="Metacurate text processing API", description="API for accessing lexical knowledge, and text normalization.")
+api = Api(api_blueprint, version="1.0", title="Metacurate text processing API",
+          description="API for accessing lexical knowledge, and text normalization.")
 
 ns = api.namespace("lookup", description="Look-up semantically similar terms.")
 
 
-# TODO how to specify valid range of parameter?
-# TODO how to get rid of the default namespace tab in swagger?
-# TODO how to document the use of defaults?
 # TODO how to style swagger?
 @ns.route("/<string:term>", defaults={"num": 10})
 @ns.route("/<string:term>/<int:num>")
 @api.doc(responses={200: "The term to look-up is available in the lexicon",
                     400: "No term to look-up is specified",
                     404: "Term not in lexicon"},
-         params={"term": "The term to look-up"})
+         params={"term": "The term to look-up in the lexicon.",
+                 "num": "The number of semantically similar terms to retrieve (optional)."})
 class LookUp(Resource):
 
     def get(self, term, num):
+        min = 1
+        max = 50
+        if num < min or num > max:
+            api.abort(400,
+                      "The number of semantically similar terms to retrieve must be "
+                      "between {} and {} (inclusive)".format(
+                          min, max))
+
         result = []
         similarities = []
         if term:
