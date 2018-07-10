a02b4297b007800c420055f8145f00341f8ba2b5
fredriko <fredrik.olsson@gmail.com>
2018-07-10 23:02:40 +0200

    Added models for collocation analysis, and the corresponding API.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 import platform
 from flask import Flask, request, render_template, g
-from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors, phrases
 
 from src.scripts import config
 from src.models.api.views import api_blueprint
 
 app = Flask(__name__)
 
+
+heroku_model_root = "/app/gensim-models/"
 
 def load_vectors():
     vectors_file_name = "word2vec-metacurate-cbow-5M-100-w10-min20-split.vectors"
@@ -15,16 +17,33 @@ def load_vectors():
         vectors_path = config.WORDSPACE_MODELS_DIRECTORY + vectors_file_name
     else:
         # Here's where heroku looks for the vectors.
-        vectors_path = "/app/gensim-models/" + vectors_file_name
+        vectors_path = heroku_model_root + vectors_file_name
     return KeyedVectors.load(vectors_path)
 
 
+def load_phrasers():
+    bigram_model_name = "bigram_phrases.model"
+    trigram_model_name = "trigram_phrases.model"
+    if platform.system() == "Darwin":
+        bigram_path = config.PHRASE_MODELS_DIRECTORY + bigram_model_name
+        trigram_path = config.PHRASE_MODELS_DIRECTORY + trigram_model_name
+    else:
+        bigram_path = heroku_model_root + bigram_model_name
+        trigram_path = heroku_model_root + trigram_model_name
+    bigrams = phrases.Phrases.load(bigram_path)
+    trigrams = phrases.Phraser.load(trigram_path)
+    return bigrams, trigrams
+
+
 MODEL = load_vectors()
+BIGRAMS, TRIGRAMS = load_phrasers()
 
 
 @app.before_request
 def before_request():
     g.metacurate_vectors = MODEL
+    g.metacurate_bigrams = BIGRAMS
+    g.metacurate_trigrams = TRIGRAMS
 
 
 @app.route("/lookup/")
--- src/models/api/views.py
+++ src/models/api/views.py
@@ -1,36 +1,51 @@
 from flask import Blueprint, g
 from flask_restplus import Resource, Api, fields
+from src.scripts.data_preparation import split_and_tokenize
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 
 api = Api(api_blueprint, version="1.0", title="Metacurate text processing API",
           description="API for accessing lexical knowledge, and text normalization.")
 
-resource_fields = api.model('lookup response', {
+lookup_response_fields = api.model("lookup response", {
     "term": fields.String(description="The term semantically similar to the one looked-up in the lexicon."),
     "similarity": fields.Float(description="The cosine similarity score of the input term and one to which this "
                                            "score is associated. Range: -1 to 1. The higher the value, the more "
                                            "semantically similar the two terms are.")
 })
 
+tokenize_response_fields = api.model("tokenize response", {
+    "sentences":
+        fields.List(
+            fields.List(
+                fields.String(description="A token. Tokens containing undescore characters, "
+                                          "i.e., _, are multi-word expressions: the underscores "
+                                          "should be considered for removal before subsequent "
+                                          "processing of the token.", required=True),
+                description="A sentence."), description="A list of sentences.")
+})
+
+parser = api.parser()
+parser.add_argument("text", type=str, required=True, help="The text to be tokenized.")
+
 
 @api.route("/lookup/<string:term>", defaults={"num": 10})
 @api.route("/lookup/<string:term>/<int:num>")
-@api.doc(responses={200: "The term to look-up is available in the lexicon",
-                    400: "No term to look-up is specified",
-                    404: "Term not in lexicon"},
+@api.doc(responses={200: "The term to look-up is available in the lexicon.",
+                    400: "No term to look-up is specified in the request.",
+                    404: "Term not in lexicon."},
          params={"term": "The term to look-up in the lexicon.",
                  "num": "The number of semantically similar terms to retrieve (optional)."})
 class LookUp(Resource):
 
-    @api.marshal_with(resource_fields, as_list=True)
+    @api.marshal_with(lookup_response_fields, as_list=True)
     def get(self, term, num):
         min = 1
         max = 50
         if num < min or num > max:
             api.abort(400,
                       "The number of semantically similar terms to retrieve must be "
-                      "between {} and {} (inclusive)".format(
+                      "between {} and {} (inclusive).".format(
                           min, max))
 
         result = []
@@ -41,22 +56,34 @@ class LookUp(Resource):
                 try:
                     similarities = g.metacurate_vectors.most_similar(positive=term.replace(" ", "_"), topn=num)
                 except KeyError:
-                    api.abort(404, "The term {} is not available in the lexicon".format(term))
+                    api.abort(404, "The term {} is not available in the lexicon.".format(term))
                 for similarity in similarities:
                     text = str(similarity[0]).replace("_", " ")
                     score = str(round(similarity[1], 2))
                     result.append({"term": text, "similarity": score})
             else:
-                api.abort(400, "No term to look-up is specified")
+                api.abort(400, "No term to look-up is specified.")
         else:
-            api.abort(400, "No term to look-up is specified")
+            api.abort(400, "No term to look-up is specified.")
         return result
 
 
-"""
 @api.route("/tokenize/")
+@api.doc(responses={200: "Processing successful.",
+                    400: "No text to tokenize is specified in the request."})
 class Tokenize(Resource):
 
+    @api.doc(parser=parser)
+    @api.marshal_with(tokenize_response_fields)
     def post(self):
-        pass
-"""
+        result = []
+        arguments = parser.parse_args()
+        text = arguments["text"]
+        text = text.strip()
+        if len(text) > 0:
+            sentences = split_and_tokenize(text)
+            for sentence in sentences:
+                result.append(list(g.metacurate_trigrams[g.metacurate_bigrams[sentence]]))
+            return {"sentences": result}, 200
+        else:
+            api.abort(400, "No text to tokenize is specified in the request.")
